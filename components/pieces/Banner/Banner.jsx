91fdfbd58f79ce43c88b9ba63b8d847ac8c7651a
Vidit <viditmodi2001@gmail.com>
2023-08-03 16:15:05 +0530

    added the logos

--- components/pieces/Banner/Banner.jsx
+++ components/pieces/Banner/Banner.jsx
@@ -16,13 +16,20 @@ const Banner = () => {
         width: 100%;
         display: flex;
         flex-direction: column;
-
+        align-items: center;
+        gap: 1rem;
         background-color: aqua;
       }
       .banner__logo__container{
-        width: 90%;
+        width: 80%;
         aspect-ratio: 1;
+        position: relative;
         background-color: blue;
+        // padding: 1rem;
+      }
+      .banner__logo{
+        width: 80%;
+        height: auto;
       }
     `}</style>
 
@@ -30,11 +37,21 @@ const Banner = () => {
     <section className={`banner`}>
       <div className="banner__logos">
         <div className="banner__logo__container">
-          <Image src={"/assets/Logo.png"} alt='logo' layout='fill' className='banner__logo'></Image>
+          <Image src={"/assets/IEM_Logo.png"} alt='iem logo' layout='fill' className='banner__logo'></Image>
+        </div>
+        <div className="banner__logo__container">
+          <Image src={"/assets/Logo.png"} alt='logo' layout='fill' objectFit="contain" className='banner__logo'></Image>
         </div>
       </div>
       <div className="banner__content"></div>
-      <div className="banner__logos"></div>
+      <div className="banner__logos">
+        <div className="banner__logo__container">
+          <Image src={"/assets/UEM_Logo.png"} alt='uem logo' layout='fill' className='banner__logo'></Image>
+        </div>
+        <div className="banner__logo__container">
+          <Image src={"/assets/Springer_Logo.png"} alt='springer logo' layout='fill' className='banner__logo'></Image>
+        </div>
+      </div>
     </section>
     </>
   )
