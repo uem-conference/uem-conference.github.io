db598655c2206b7ca102bfebda821192be7b4470
Vidit <viditmodi2001@gmail.com>
2023-08-04 03:16:05 +0530

    all the pages made for now

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,11 +1,182 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {Heading} from "../../components/pieces"
 
-const index = () => {
-  return (
-    <div>
-      contact
-    </div>
-  )
-}
+const ContactPage = () => {
+	useEffect(() => {
+		document.title = "Contact | " + process.env.REACT_APP_TITLE;
+	}, []);
+	function clickMailHandler() {
+		const email = "adcomsys@uem.edu.in";
+		const subject = "Regarding AdComSys Conference";
+		const body =
+			"Hello, I would like to inquire about the AdComSys Conference.";
 
-export default index
+		const mailtoUrl = `mailto:${email}?subject=${encodeURIComponent(
+			subject
+		)}&body=${encodeURIComponent(body)}`;
+
+		window.location.href = mailtoUrl;
+	}
+
+	return (
+		<>
+    <style jsx>{`
+    .contact__grid {
+      display: grid;
+      grid-template-columns: 2fr 3fr;
+      gap: 2rem;
+    }
+    
+    .contact__map {
+      width: 100%;
+      height: 100%;
+      /* background-color: aqua; */
+    }
+    .contact__details {
+      width: 100%;
+      height: 100%;
+      padding: 1rem 2rem;
+      /* background-color: aqua; */
+    }
+    .contact__map iframe {
+      width: 100%;
+      height: 100%;
+      /* background-color: aqua; */
+    }
+    
+    .contact__icon {
+      width: 2rem;
+      height: auto;
+    }
+    
+    .phone-icon {
+      width: 1.5rem;
+    }
+    
+    .contact__text {
+      font-size: var(--font-size-text-small);
+      line-height: calc(var(--font-size-text-small) * 1.35);
+      margin-bottom: 1rem;
+    }
+    
+    .contact__container {
+      display: flex;
+      gap: 1rem;
+      align-items: center;
+    }
+    
+    /* extraa added later */
+    .email:hover {
+      text-decoration: underline;
+    }
+    .email-icon:hover {
+      zoom: 120%;
+    }
+    
+    `}</style>
+			<Heading text={"Contact"} class={"mb--2 invalid-selection"}></Heading>
+			<div className="contact__grid">
+				<div className="contact__details">
+					<p className="contact__heading text text--normal">
+						Prof. Dr. Maumita Chakraborty
+					</p>
+
+					<p className="contact__text text text--normal">
+						Convener, AdComSys 2024{" "}
+					</p>
+					<div className="contact__container">
+          <span class="material-symbols-outlined contact__icon address-icon">
+map
+</span>
+						{/* <img
+							src={address_icon}
+							alt="Address"
+							className="contact__icon address-icon"
+						/> */}
+						<p className="contact__text text text--normal">
+							University Area, Plot No. III – B/5, New Town, Action Area – III,
+							Kolkata, West Bengal 700156{" "}
+						</p>
+					</div>
+
+					<p className="contact__text text text--normal">
+						<strong>For details you may contact the following:</strong>
+					</p>
+					<div className="contact__container">
+          <span class="material-symbols-outlined contact__icon phone-icon">
+phone_in_talk
+</span>
+						{/* <img
+							src={phone_icon}
+							alt="phone"
+							className="contact__icon phone-icon"
+							style={{ marginBottom: "5px" }}
+						/> */}
+						<p
+							style={{ margin: "0", marginBottom: "5px" }}
+							className="contact__text text text--normal"
+						>
+							Dr. Chiradeep Mukherjee - 9804244362
+						</p>
+					</div>
+					<div className="contact__container">
+          <span class="material-symbols-outlined contact__icon phone-icon">
+phone_in_talk
+</span>
+						{/* <img
+							src={phone_icon}
+							alt="phone"
+							className="contact__icon phone-icon"
+						/> */}
+						<p
+							style={{ margin: "0", marginBottom: "5px" }}
+							className="contact__text text text--normal"
+						>
+							Dr. Subhalaxmi Chakraborty - 7003720703
+						</p>
+					</div>
+					{/* <p className="contact__text text text--normal">
+            , Phone No.: 
+          </p> */}
+					{/* <p className="contact__text text text--normal">
+            , Phone No.: 
+          </p> */}
+
+					<div onClick={clickMailHandler} className="contact__container">
+          <span class="material-symbols-outlined contact__icon email-icon">
+mail
+</span>
+						{/* <img
+							src={email_icon}
+							alt="Email"
+							className="contact__icon email-icon"
+							style={{ marginTop: "5px" }}
+						/> */}
+
+						<p
+							style={{ margin: "0", marginTop: "5px" }}
+							className="contact__text text text--normal email"
+						>
+							adcomsys@uem.edu.in
+						</p>
+					</div>
+				</div>
+
+				<div className="contact__map">
+					<iframe
+						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.512563514172!2d88.4873264744012!3d22.559925133436433!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a020b267a3cdc13%3A0xb3b21d652126f40!2sUniversity%20of%20Engineering%20%26%20Management%2C%20Kolkata!5e0!3m2!1sen!2sin!4v1689485233150!5m2!1sen!2sin"
+						// width="600"
+						// height="450"
+						style={{ border: 0 }}
+						allowFullScreen=""
+						title="Map"
+						// loading="lazy"
+						referrerPolicy="no-referrer-when-downgrade"
+					></iframe>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default ContactPage;
--- pages/registration/index.jsx
+++ pages/registration/index.jsx
@@ -1,11 +1,151 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {Heading} from "../../components/pieces"
 
-const index = () => {
-  return (
-    <div>
-      register
-    </div>
-  )
-}
+const RegistrationPage = () => {
+	useEffect(() => {
+		document.title = "Registration | " + process.env.REACT_APP_TITLE;
+	}, []);
+	return (
+		<>
+    <style jsx>{`
+    .registration-page {
+      /* width: 100%; */
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    
+    .registration-page > * {
+      width: 100%;
+    }
+    
+    
+    @media screen and (max-width: 768px) {
+      
+    }
+    
+    @media screen and (max-width: 450px) {
+      /* .registration-page{
+        padding: 2rem;
+      } */
+    }
+    `}</style>
+			<Heading
+				text={"Registration Policy"}
+				class={"mb--2 invalid-selection"}
+			></Heading>
+			<div className="registration-page content-container">
+				<p className="text text--normal mb--1">
+					According to the AdComSys 2024 conference registration policy, after
+					obtaining the formal acceptance by e-mail, at least one author of each
+					paper must complete registration formalities including payment of full
+					registration fees.
+				</p>
 
-export default index
+				<table border={"0"} className="table my--2">
+					<thead className="table__head heading">
+						<tr className="table__row">
+							<th className="table__heading">Category</th>
+							<th className="table__heading">
+								Registration fees for Indian Author
+							</th>
+							<th className="table__heading">
+								Registration fees for foreigner
+							</th>
+						</tr>
+					</thead>
+					<tbody className="table__body text text--normal">
+						<tr className="table__row">
+							<td className="table__data">Regular Author Academician</td>
+							<td className="table__data">INR 4000</td>
+							<td className="table__data">150 USD</td>
+						</tr>
+						<tr className="table__row">
+							<td className="table__data">Regular Author from Industry</td>
+							<td className="table__data">INR 6000</td>
+							<td className="table__data">200 USD</td>
+						</tr>
+						<tr className="table__row">
+							<td className="table__data">Regular Author Student (Phd)</td>
+							<td className="table__data">INR 4000</td>
+							<td className="table__data">150 USD</td>
+						</tr>
+						<tr className="table__row">
+							<td className="table__data">Regular Author Student (PG)</td>
+							<td className="table__data">INR 3500</td>
+							<td className="table__data">120 USD</td>
+						</tr>
+						<tr className="table__row">
+							<td className="table__data">Regular Author Student (UG)</td>
+							<td className="table__data">INR 3500</td>
+							<td className="table__data">120 USD</td>
+						</tr>
+						<tr className="table__row">
+							<td className="table__data">Extra Paper</td>
+							<td className="table__data">INR 2500</td>
+							<td className="table__data">100 USD</td>
+						</tr>
+						<tr className="table__row">
+							<td className="table__data">Only Attending or Accompanying</td>
+							<td className="table__data">INR 1500</td>
+							<td className="table__data">90 USD</td>
+						</tr>
+					</tbody>
+				</table>
+
+				<p className="text text--normal mb--1">
+					Final submissions can only be uploaded with regular registration and
+					at least one author of each paper must register at the regular rate.
+				</p>
+				<p className="text text--normal mb--1">
+					This fee is only applicable for single author from each paper who will
+					be coming to present his/her paper in the respective venue of the
+					conference. For each paper, if more than one author will come to
+					present the paper then INR 2000 (Indian author) or 100 USD(for the
+					foreign author) will be paid by each author at the conference venue
+					(this includes registration kit, breakfast, and lunch coupon for three
+					days of the conference ).
+				</p>
+				<p className="text text--normal mb--1">
+					The above registration fees include tickets for the meals (Admission
+					in opening ceremony, registration kit, breakfasts and lunches) for
+					each author and one copy of the proceeding on a pen drive.
+				</p>
+
+				<h4
+					className="heading heading--3 mb--1"
+					style={{ textTransform: "uppercase" }}
+				>
+					Paper Registration Link
+				</h4>
+
+				<p className="text text--normal mb--1">
+					To register your Paper please <a href="/">Click here.</a>
+				</p>
+
+				<p className="text text--normal mb--1">
+					After your paper get registered, the author will get an auto-generated
+					e-mail, the auto-generated e-mail consist of an invoice having the
+					amount you have paid to register your paper. Author has to e-mail that
+					invoice to conference e-mail id (adcomsys@uem.edu.in) along with their
+					paper id and paper title. If any author will not send the invoice to
+					the conference email-id then their registration will not be granted or
+					treated as invalid.
+				</p>
+
+				<h4
+					className="heading heading--3 mb--1"
+					style={{ textTransform: "uppercase" }}
+				>
+					Cancellation Policy
+				</h4>
+
+				<p className="text text--normal mb--1">
+					There will be no cancellation after Registration
+				</p>
+			</div>
+		</>
+	);
+};
+
+export default RegistrationPage;
