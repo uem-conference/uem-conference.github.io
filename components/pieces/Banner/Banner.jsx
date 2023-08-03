e50215feceeb42bb6d4a1db7b2e9385bd11c53a7
Vidit <viditmodi2001@gmail.com>
2023-08-03 15:59:15 +0530

    added the google fonts

--- components/pieces/Banner/Banner.jsx
+++ components/pieces/Banner/Banner.jsx
@@ -6,12 +6,35 @@ const Banner = () => {
   return (
     <>
     <style jsx>{`
-      
+      .banner{
+        width: 100%;
+        background-color: aqua;
+        display: grid;
+        grid-template-columns: 1fr 4fr 1fr;
+      }
+      .banner__logos{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+
+        background-color: aqua;
+      }
+      .banner__logo__container{
+        width: 90%;
+        aspect-ratio: 1;
+        background-color: blue;
+      }
     `}</style>
 
 
-    <section className={`${styles.banner}`}>
-      Banner
+    <section className={`banner`}>
+      <div className="banner__logos">
+        <div className="banner__logo__container">
+          <Image src={"/assets/Logo.png"} alt='logo' layout='fill' className='banner__logo'></Image>
+        </div>
+      </div>
+      <div className="banner__content"></div>
+      <div className="banner__logos"></div>
     </section>
     </>
   )
