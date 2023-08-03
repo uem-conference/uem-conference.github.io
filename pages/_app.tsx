ea8d8fab9f4842bf494dfd2a56f330fdef0df0f5
Ankur Halder <ankur.halder12345@gmail.com>
2023-08-03 19:20:40 +0530

    small bugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import BodyTemplate from "../components/templates/BodyTemplate"
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import BodyTemplate from "../components/templates/BodyTemplate";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <BodyTemplate child= {<Component {...pageProps} />}></BodyTemplate>
+	return <BodyTemplate child={<Component {...pageProps} />}></BodyTemplate>;
 }
