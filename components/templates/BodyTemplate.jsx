92b5711afefedc215c644a3dbd1e76ef54acba60
Ankur Halder <ankur.halder12345@gmail.com>
2023-08-04 15:49:25 +0530

    Dialog chatbot successfully intragated

--- components/templates/BodyTemplate.jsx
+++ components/templates/BodyTemplate.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import styles from "./bodyTemplate.module.css";
 import Head from "next/head";
 import { Header, Footer } from "../containers";
-
+import { ChatBot } from "../pieces";
 const BodyTemplate = (props) => {
 	return (
 		<>
+			<ChatBot></ChatBot>
 			<Header></Header>
-			<main className="wrapper" style={{marginBottom: "4rem"}}>{props.child}</main>
+			<main className="wrapper" style={{ marginBottom: "4rem" }}>
+				{props.child}
+			</main>
 			<Footer></Footer>
 		</>
 	);
