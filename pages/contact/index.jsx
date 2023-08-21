379991ee396f79e2ec5bf283caa4e95266c2f5a7
Ankur Halder <ankur.halder12345@gmail.com>
2023-08-21 13:45:05 +0530

    in contact meta tag added

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { Heading } from "../../components/pieces";
 import Image from "next/image";
+import Head from "next/head";
 const ContactPage = () => {
 	useEffect(() => {
 		document.title = "Contact | " + process.env.app_name;
@@ -20,6 +21,41 @@ const ContactPage = () => {
 
 	return (
 		<>
+			<Head>
+				<title>Contact | Your Conference Name</title>
+				<meta
+					name="description"
+					content="Get in touch with the organizers of the conference."
+				/>
+
+				<meta property="og:title" content="Contact" />
+				<meta
+					property="og:description"
+					content="Get in touch with the organizers of the conference."
+				/>
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://adcomsys.vercel.app/contact" />
+				<meta
+					property="og:image"
+					content="https://adcomsys.vercel.app/_next/image?url=%2Fassets%2FLogo.png&w=1920&q=75"
+				/>
+				<meta property="og:image:alt" content="Contact" />
+				<meta property="og:locale" content="en_US" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+				<meta name="author" content="Ankur Halder" />
+				<meta name="robots" content="index, follow" />
+				<meta name="language" content="English" />
+				<meta charSet="UTF-8" />
+				<link rel="icon" href="/favicon.ico" />
+
+				<meta name="geo.region" content="IN-WB" />
+				<meta name="geo.placename" content="Kolkata, India" />
+				<meta name="geo.position" content="22.572645;88.363892" />
+				<meta name="ICBM" content="22.572645, 88.363892" />
+
+				<meta http-equiv="content-language" content="en" />
+			</Head>
 			<style jsx>{`
 				.contact__grid {
 					display: grid;
