eb12611cb66924715a36477ec6369105cb329bbc
Ankur Halder <ankur.halder12345@gmail.com>
2024-02-02 00:01:26 +0530

    modules updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import "../styles/chatbot.css";
 import "../styles/footer_v2.scss";
 import type { AppProps } from "next/app";
 import BodyTemplate from "../components/templates/BodyTemplate";
-import { Analytics } from "@vercel/analytics/react";
 import { Fragment } from "react";
 // import firebase from 'firebase/app';
 
@@ -11,7 +10,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <BodyTemplate child={<Component {...pageProps} />}></BodyTemplate>
-      <Analytics />
     </Fragment>
   );
 }
