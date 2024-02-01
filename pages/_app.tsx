3eb6dc203678c534dcadefdb56eaa4fffa4d0f49
Ankur Halder <ankur.halder12345@gmail.com>
2024-02-01 23:48:21 +0530

    now vercel analitics should work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import "../styles/footer_v2.scss";
 import type { AppProps } from "next/app";
 import BodyTemplate from "../components/templates/BodyTemplate";
 import { Analytics } from "@vercel/analytics/react";
+import { Fragment } from "react";
 // import firebase from 'firebase/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <BodyTemplate child={<Component {...pageProps} />}>
+    <Fragment>
+      <BodyTemplate child={<Component {...pageProps} />}></BodyTemplate>
       <Analytics />
-    </BodyTemplate>
+    </Fragment>
   );
 }
