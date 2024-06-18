1f3731087699cce36fc17423c92f8663d6415106
Ankur Halder <ankur.halder12345@gmail.com>
2024-06-18 15:16:18 +0530

    chore: Add meta tags and favicon to improve SEO and user experience

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,20 @@ import "@/styles/globals.css";
 import "../styles/chatbot.css";
 import "../styles/footer_v2.scss";
 import "/components/pieces/KeynoteCard/keynote_card.scss";
-
-// import "../styles/base/certificate.scss";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import BodyTemplate from "../components/templates/BodyTemplate";
 import { Fragment } from "react";
-// import firebase from 'firebase/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="adcomsys official website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/assets/Logo.png" />
+      </Head>
       <BodyTemplate child={<Component {...pageProps} />}></BodyTemplate>
     </Fragment>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* <meta name="description" content="adcomsys official website" /> */}
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
-        <link rel="icon" href="/assets/Logo.png" />
-      </Head>
-
+      <Head />
       <body>
         <Main />
         <NextScript />
